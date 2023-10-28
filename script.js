6946ba8588f63f70136a98a9660962197e18fe96
lenyyluz <cristian.leny@gmail.com>
2023-10-27 21:44:11 -0300

    Validación de formulario con Javascript y html

---
+++ script.js
@@ -0,0 +1,29 @@
+// Script de Bootstrap para validar formularios
+
+// Example starter JavaScript for disabling form submissions if there are invalid fields
+
+
+
+// script para recibir los datos del formulario en Formspree que lo desactivamos porque no pudimos hacerlo funcionar.
+
+
+const $form = document.querySelector('#form')
+
+$form.addEventListener('submit', handleSubmit)
+
+async function handleSubmit(event){
+    event.preventDefault()
+    const form = new formData(this)
+    const response = fetch(this.action,{
+        method: this.method,
+        body: form, 
+        headers:{
+            'Accept': 'application/json'
+        }
+    })
+    if (response.ok){
+        this.reset()
+        alert('Gracias por contactarnos, te enviaremos pronto más novedades')
+    }
+}
+
